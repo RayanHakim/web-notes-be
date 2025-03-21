33517b1a307e65743bb0f130b1d6dd454f18cd08
plirapli <rafliyulistiawan@gmail.com>
2025-03-21 15:36:52 +0700

    gatau

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('notes', 'root', '', {
-    host: '34.28.99.134',
-    dialect: 'mysql'
-}); 
+const db = new Sequelize("notes", "root", "", {
+  host: "34.28.99.134",
+  dialect: "mysql",
+});
 
 export default db;
