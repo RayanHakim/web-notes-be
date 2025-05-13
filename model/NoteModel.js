fcbcf5b8cb9193b3b0616b86531c55905dd76a93
RayanHakim <161198754+RayanHakim@users.noreply.github.com>
2025-05-13 22:36:42 +0700

    Update NoteModel.js

--- model/NoteModel.js
+++ model/NoteModel.js
@@ -1,18 +1,22 @@
 import { Sequelize } from "sequelize";
-import db from "../config/Database.js"
+import db from "../config/Database.js";
 
-const Note = db.define('note', {
+// Membuat tabel "note"
+const Note = db.define(
+  "note", // Nama Tabel
+  {
     title: Sequelize.STRING,
-    content: Sequelize.STRING,},{
-        freezeTableName: true,
-        createdAt: 'tanggal_buat',
-        updatedAt: 'tanggal_ubah',
-        password: Sequelize.STRING,
-        refresh_token: Sequelize.TEXT
-});
+    content: Sequelize.STRING,
+    password: Sequelize.STRING,
+    refresh_token: Sequelize.TEXT
+  },
+  {
+    freezeTableName: true,
+    createdAt: "tanggal_buat",
+    updatedAt: "tanggal_ubah"
+  }
+);
+
+db.sync().then(() => console.log("Database synced"));
 
 export default Note;
-
-(async() => {
-    await db.sync();
-})();
