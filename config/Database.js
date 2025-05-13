80bd89bb7145ab03ed6cd66dd7ba4f96079494e6
RayanHakim <161198754+RayanHakim@users.noreply.github.com>
2025-05-13 22:06:07 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,14 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv"; 
 
-const db = new Sequelize("notes", "root", "rayan123", {
-  host: "34.28.99.134",
+dotenv.config();
+
+const DB_NAME = process.env.DB_NAME;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+const db = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: "mysql",
 });
 
