3e6d3c7a55924fa8397ce07577cad0a6db0a6fcf
RayanHakim <161198754+RayanHakim@users.noreply.github.com>
2025-05-19 12:08:13 +0700

    Update database.js

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: 34.28.99.134,
+    host: process.env.DB_HOST,       // Ambil dari .env
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
