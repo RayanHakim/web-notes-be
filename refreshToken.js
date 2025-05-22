686443f84f8abf64480c52ae42560c89348381cb
RayanHakim <161198754+RayanHakim@users.noreply.github.com>
2025-05-22 21:14:11 +0700

    Update refreshToken.js

--- refreshToken.js
+++ refreshToken.js
@@ -1,26 +1,28 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 let refreshTokens = [];
 
 exports.generateRefreshToken = (user) => {
-    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
-    refreshTokens.push(refreshToken);
-    return refreshToken;
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "1d",
+  });
+  refreshTokens.push(refreshToken);
+  return refreshToken;
 };
 
 exports.verifyRefreshToken = (req, res) => {
-    const { token } = req.body;
-    if (!token || !refreshTokens.includes(token)) return res.sendStatus(403);
+  const { token } = req.body;
+  if (!token || !refreshTokens.includes(token)) return res.sendStatus(403);
 
-    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
+  jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
 
-        const accessToken = jwt.sign(
-            { id: user.id, username: user.username },
-            process.env.REFRESH_TOKEN_SECRET, //  Semoga ini kesalahannya dah
-            { expiresIn: '15m' }
-        );
-        res.json({ accessToken });
-    });
+    const accessToken = jwt.sign(
+      { id: user.id, username: user.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "15m" }
+    );
+    res.json({ accessToken });
+  });
 };
