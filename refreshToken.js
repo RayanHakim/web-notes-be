1d1e21b8960bc4c13e61ce07ccde4a87e7324da4
RayanHakim <161198754+RayanHakim@users.noreply.github.com>
2025-05-22 20:51:05 +0700

    Update refreshToken.js

--- refreshToken.js
+++ refreshToken.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 let refreshTokens = [];
 
 exports.generateRefreshToken = (user) => {
-    const refreshToken = jwt.sign(user, process.env.JWT_REFRESH_SECRET, { expiresIn: '1d' });
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
     refreshTokens.push(refreshToken);
     return refreshToken;
 };
@@ -13,12 +13,12 @@ exports.verifyRefreshToken = (req, res) => {
     const { token } = req.body;
     if (!token || !refreshTokens.includes(token)) return res.sendStatus(403);
 
-    jwt.verify(token, process.env.JWT_REFRESH_SECRET, (err, user) => {
+    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
         if (err) return res.sendStatus(403);
 
         const accessToken = jwt.sign(
             { id: user.id, username: user.username },
-            process.env.JWT_SECRET,
+            process.env.ACCESS_TOKEN_SECRET, //  Semoga ini kesalahannya dah
             { expiresIn: '15m' }
         );
         res.json({ accessToken });
